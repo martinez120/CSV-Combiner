e1335c67901afd7181795108de99cd9e8152f4fb
Martinez_120 <50432483+martinez120@users.noreply.github.com>
2022-03-31 03:12:29 -0500

    Add files via upload

---
+++ Alternative/combined.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import numpy as np
+import glob
+import os
+import csv
+
+def csv_edit(input_path):
+
+    path = input_path
+    csv_files = glob.glob(os.path.join(path, "*.csv"))
+
+
+    for f in csv_files:
+    
+        df = pd.read_csv(f)
+        name = f.split("\\")[-1]
+        
+        df.insert (0, column = "Filename", value = name)
+        df.head()
+
+        df.to_csv(f, header = 2, index = False)
+
+
+
+def csv_combine(input_path):
+
+    csv_edit(input_path)
+    output_path = input_path + "/Combined.csv"
+    
+    path = input_path
+    all_files = glob.glob(path + "/*.csv")
+    
+
+    file_list = [pd.read_csv(filename, index_col = None, header = 0) for filename in all_files]
+    df = pd.concat(file_list, axis = 0, ignore_index = True)
+
+
+    df.to_csv(output_path, header = 2, index = False)
+
+    print("Task Completed")
+
+
+def main():
+
+    print("Note: Make sure that backslashes \ are forwardslashes / instead")
+    file_location = input("Enter Csv file location: ")
+    
+    csv_combine(file_location)
+
+
+if __name__ == "__main__":
+    main()
